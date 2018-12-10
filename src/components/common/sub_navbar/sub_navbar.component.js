b08ac08effeb611782c7d9c189e8a161445bc406
devilcrucifier <amitbvp13@gmail.com>
2018-12-11 04:07:01 +0530

    Warning fixes

--- src/components/common/sub_navbar/sub_navbar.component.js
+++ src/components/common/sub_navbar/sub_navbar.component.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { List } from "antd";
 import { SubNavMenu, MenuItem } from "./sub_navbar.style";
-// import { fetchCategory } from "../../../_actions/subNavBar.action";
 import category from "../../../_assets/category";
-import SiderComponent from "../sidebar/sidebar.component";
 import { fetchSubCategory } from "../../../_actions/subNavBar.action";
 
 class SubNavbarComponent extends React.Component {
@@ -13,7 +10,6 @@ class SubNavbarComponent extends React.Component {
   };
 
   handleClick = e => {
-    console.log("click ", e);
     this.setState({
       current: e.key
     });
